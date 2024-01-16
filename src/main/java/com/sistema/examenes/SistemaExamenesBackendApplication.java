e2f900913999701aea8970b94c1d98414c76c5c3
Matias Criollo <matiascriollo57@gmail.com>
2024-01-16 00:13:51 -0500

    Probando el metodo para guardar un usuario agregando un usuario

--- src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
+++ src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
@@ -1,13 +1,50 @@
 package com.sistema.examenes;
 
+import com.sistema.examenes.entities.Rol;
+import com.sistema.examenes.entities.Usuario;
+import com.sistema.examenes.entities.UsuarioRol;
+import com.sistema.examenes.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SpringBootApplication
-public class SistemaExamenesBackendApplication {
+public class SistemaExamenesBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	public UsuarioService usuarioService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaExamenesBackendApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Matias");
+		usuario.setApellido("Criollo Vigo");
+		usuario.setUsername("Mackend");
+		usuario.setPasswrod("12345");
+		usuario.setEmail("mackend@gmail.com");
+		usuario.setTelefono("967354987");
+		usuario.setPerfil("foto.png");
+
+		Rol rol = new Rol();
+		rol.setRolId(1L);
+		rol.setNombre("ADMIN");
+
+		Set<UsuarioRol> usuarioRoles = new HashSet<>();
+		UsuarioRol usuarioRol = new UsuarioRol();
+		usuarioRol.setRol(rol);
+		usuarioRol.setUsuario(usuario);
+		usuarioRoles.add(usuarioRol);
+
+		Usuario usuarioGuardado = usuarioService.guardarUsuario(usuario, usuarioRoles);
+		System.out.println(usuarioGuardado.getUsername());
+	}
 }
