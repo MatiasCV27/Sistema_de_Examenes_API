f9cf1af70a1f7d34ed9f592361460cde0b8b0253
Matias Criollo <matiascriollo57@gmail.com>
2024-01-16 16:04:57 -0500

    Creando el controllador de usuarios

---
+++ src/main/java/com/sistema/examenes/controller/UsuarioController.java
@@ -0,0 +1,45 @@
+package com.sistema.examenes.controller;
+
+import com.sistema.examenes.entities.Rol;
+import com.sistema.examenes.entities.Usuario;
+import com.sistema.examenes.entities.UsuarioRol;
+import com.sistema.examenes.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/")
+    public Usuario guardarUsuario(@RequestBody Usuario usuario) throws Exception {
+        Set<UsuarioRol> roles = new HashSet<>();
+
+        Rol rol = new Rol();
+        rol.setRolId(2L);
+        rol.setNombre("NORMAL");
+
+        UsuarioRol usuarioRol = new UsuarioRol();
+        usuarioRol.setUsuario(usuario);
+        usuarioRol.setRol(rol);
+
+        return usuarioService.guardarUsuario(usuario, roles);
+    }
+
+    @GetMapping("/{username}")
+    public Usuario obtenerUsuario(@PathVariable("username") String username) {
+        return usuarioService.obtenerUsuario(username);
+    }
+
+    @DeleteMapping("/{usuarioId}")
+    public Usuario eliminarUsuario(@PathVariable("usuarioId") Long usuarioId) {
+        usuarioService.eliminarUsuario(usuarioId);
+    }
+
+}
--- src/main/java/com/sistema/examenes/services/UsuarioService.java
+++ src/main/java/com/sistema/examenes/services/UsuarioService.java
@@ -7,6 +7,10 @@ import java.util.Set;
 
 public interface UsuarioService {
 
-    public Usuario guardarUsuario(Usuario usuario, Set<UsuarioRol> usuarioRoles) throws Exception;
+    Usuario guardarUsuario(Usuario usuario, Set<UsuarioRol> usuarioRoles) throws Exception;
+
+    Usuario obtenerUsuario(String username);
+
+    void eliminarUsuario(Long usuarioId);
 
 }
