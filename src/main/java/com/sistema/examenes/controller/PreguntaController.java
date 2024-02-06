0db160ea98f53ab4cf9ae12a693ca0141d247df4
Matias Criollo <matiascriollo57@gmail.com>
2024-02-05 23:49:13 -0500

    Creación del RestController de Preguntas

---
+++ src/main/java/com/sistema/examenes/controller/PreguntaController.java
@@ -0,0 +1,58 @@
+package com.sistema.examenes.controller;
+
+import com.sistema.examenes.entities.Examen;
+import com.sistema.examenes.entities.Pregunta;
+import com.sistema.examenes.services.ExamenService;
+import com.sistema.examenes.services.PreguntaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/pregunta")
+@CrossOrigin("*")
+public class PreguntaController {
+
+    @Autowired
+    private PreguntaService preguntaService;
+
+    @Autowired
+    private ExamenService examenService;
+
+    @PostMapping("/")
+    public ResponseEntity<Pregunta> guardarPregunta(@RequestBody Pregunta pregunta) {
+        return ResponseEntity.ok(preguntaService.agregarPregunta(pregunta));
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Pregunta> actualizarPregunta(@RequestBody Pregunta pregunta) {
+        return ResponseEntity.ok(preguntaService.actualizarPregunta(pregunta));
+    }
+
+    @GetMapping("/examen/{examenId}")
+    private ResponseEntity<?> listarPreguntasDelExamen(@PathVariable("examenId") Long examenId) {
+        Examen examen = examenService.obtenerExamen(examenId);
+        Set<Pregunta> preguntas = examen.getPreguntas();
+
+        List examenes = new ArrayList(preguntas);
+        if (examenes.size() > Integer.parseInt(examen.getNumeroDePreguntas())) {
+            examenes = examenes.subList(0, Integer.parseInt(examen.getNumeroDePreguntas() + 1));
+        }
+
+        Collections.shuffle(examenes);
+        return ResponseEntity.ok(examenes);
+    }
+
+    @GetMapping("/{preguntaId}")
+    public Pregunta listarPreguntaPorId(@PathVariable("preguntaId") Long preguntaId) {
+        return preguntaService.obtenerPregunta(preguntaId);
+    }
+
+    @DeleteMapping("/{preguntaId}")
+    private void eliminarPregunta(@PathVariable("preguntaId") Long preguntaId) {
+        preguntaService.eliminarPregunta(preguntaId);
+    }
+
+}
